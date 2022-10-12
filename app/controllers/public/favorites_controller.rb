30075bc64da12eae5722ed96c7f494f3132f6e1d
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-10-12 06:33:25 +0000

    10/12~middle-layouts

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,16 @@
 class Public::FavoritesController < ApplicationController
-  
+
   def create
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_customer.favorites.new(post_image_id: post_image.id)
     favorite.save
-    redirect_to post_image_path(post_image)
+    redirect_to request.referer
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_customer.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
-    redirect_to post_image_path(post_image)
+    redirect_to request.referer
   end
 end
