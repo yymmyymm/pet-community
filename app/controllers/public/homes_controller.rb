7035c0652c619993e52ce774b242c80f5e073f23
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-10-23 06:09:11 +0000

    fixed deploy error

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,9 @@ class Public::HomesController < ApplicationController
 
   def top
     @post_images = PostImage.all
-    @post_image = PostImage.order("RANDOM()").limit(1)
+    if @post_images != nil
+      @post_image = @post_images[SecureRandom.random_number(PostImage.all.count())]
+    end
     @customers = Customer.all
     @genres = Genre.all
     @tag_list=Tag.all
